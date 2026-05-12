a199b99d37619eb6720fccad6dfd63a8cd2d8320
game0verxx <linasbutkus6@gmail.com>
2026-05-12 22:47:36 +0200

    req-2 and req-3

--- remaining_time/config.py
+++ remaining_time/config.py
@@ -4,3 +4,4 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RAW_DIR = os.path.join(PROJECT_ROOT, "data", "raw")
 PROCESSED_DIR = os.path.join(PROJECT_ROOT, "data", "processed")
 XES_FILE_PATH = os.path.join(RAW_DIR, "BPI_Challenge_2013_incidents.xes.gz")
+REQUIRED_COLUMNS = ["case:concept:name", "concept:name", "time:timestamp"]
--- remaining_time/data_loader.py
+++ remaining_time/data_loader.py
@@ -1,8 +1,21 @@
 import pm4py
-from config import XES_FILE_PATH
+from config import XES_FILE_PATH,  REQUIRED_COLUMNS
 
 def load_data():
     print("Loading log...")
     log = pm4py.read_xes(XES_FILE_PATH)
     print("Log loaded successfully.")
+    return log
+
+def validate_columns(log, required_columns):
+    print("Validating columns...")
+    for column in required_columns:
+        if column not in log.columns:
+            raise ValueError(f"Column '{column}' is missing from log.")
+    print("Columns validated successfully.")
+
+def sort_cases_by_timestamp(log, case_id_col, timestamp_col):
+    print("Sorting cases by timestamp...")
+    log.sort_values([case_id_col, timestamp_col], ascending=True, inplace=True)
+    print("Cases sorted successfully.")
     return log
