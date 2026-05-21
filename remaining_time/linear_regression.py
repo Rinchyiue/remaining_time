6cb28856b4f76206c7f5cbb66383c49777ff3e7e
Rinchyiue <rinj.qifanwu@gmail.com>
2026-05-21 12:10:42 +0200

    deleted unused comments in linear_regression

--- remaining_time/linear_regression.py
+++ remaining_time/linear_regression.py
@@ -1,20 +1,4 @@
-# preprocessed dataset (prefix set) should be imported
 # mathematical notation: y(w,x) = w0 + w1x1 + ... + wnxn
-# suggested features (which determines n):
-#   baseline:
-#           - time (derived duration)
-#               - total duration before the inspected activity since start
-#               - time since the inspected activity started
-#               - duration of the inspected activity
-#               - total duration after the inspected activity till completion
-#           - trace (only prefix for now)
-#               - prefix traces (merely variances)
-#           - case ID
-#           - case level attributes (e.g. cost, resource ...)
-#   optional:
-#           - event level attributes, esp. those of the inspected activity
-#           - combine classification methods -> group and label the prefix traces (not merely absolute variances) => Pipeline
-#           - more process respectives (e.g. resource sequence/combination ...)
 # goal: find out "good" coefficients w = (w1,...,wn) and intercept w0
 
 import numpy as np
