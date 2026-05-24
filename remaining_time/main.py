74827263f5115a78052540f5631bed35a37df013
game0verxx <linasbutkus6@gmail.com>
2026-05-24 19:05:59 +0200

    Pipeline

--- remaining_time/main.py
+++ remaining_time/main.py
@@ -1,17 +1,21 @@
 """
 A module for the pipeline of the system.
 """
+import pm4py
+import pandas as pd
+
 from config import REQUIRED_COLUMNS
 from data_loader import load_data, validate_columns, sort_cases_by_timestamp, filter_completed_cases
 from prefix_extractor import compute_remaining_time, filter_short_prefixes
 from data_splitter import time_based_split
+from feature_engineering import extract_static_case_attr, extract_aggr_dynamic_features, extract_temporal_features
 
 def main():
     print("--- Starting pipeline for remaining time ---")
 
     # 1. Loading the data
     log = load_data()
-
+    
     # 2. Validating the existence of the essential columns within the data
     validate_columns(log, REQUIRED_COLUMNS)
 
@@ -27,13 +31,16 @@ def main():
     # 6. Filtering out cases with less than min_length events
     log = filter_short_prefixes(log, REQUIRED_COLUMNS[0], min_length=2)
 
-    # 7. Performing time-based data split (70% training, 15% validation and testing)
+    # 7. Feature Engineering
+    # 7.1. Extracting static case attributes
+    log = extract_static_case_attr(log, REQUIRED_COLUMNS[0], ["impact", "product", "organization involved"])
+    # 7.2. Extracting aggregated dynamic features
+    log = extract_aggr_dynamic_features(log, REQUIRED_COLUMNS[0], REQUIRED_COLUMNS[2], REQUIRED_COLUMNS[1])
+    # 7.3. Extracting temporal features
+    log = extract_temporal_features(log, REQUIRED_COLUMNS[2])
+
+    # 8. Performing time-based data split (70% training, 15% validation and testing)
     train_log, val_log, test_log = time_based_split(log, REQUIRED_COLUMNS[0], REQUIRED_COLUMNS[2])
-
-    # display of essential columns for testing purposes
-    display_columns = [REQUIRED_COLUMNS[0], REQUIRED_COLUMNS[1], REQUIRED_COLUMNS[2], "remaining_time"]
-    print(log[display_columns].head())
-
 
 if __name__ == "__main__":
     main()
