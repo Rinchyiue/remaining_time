7aeda8f19013cb02685d1d24eb8317a6d82f344a
Rinchyiue <rinj.qifanwu@gmail.com>
2026-05-26 12:58:28 +0200

    deleted index functions in score_management.py

--- remaining_time/score_management.py
+++ remaining_time/score_management.py
@@ -21,22 +21,6 @@ def getBest(df):
 
 # @para df:
 #       type: pandas.DataFrame
-#       content: the data frame to be looked after
-# @para column:
-#       type: str
-#       content: target column name
-# @para value:
-#       type: object
-#       content: the target value in the column
-# @output:
-#       type: pandas.core.indexes.numeric.Int64Index
-#       content: a sequence of indexes which fulfils df[column] == value
-# functionality: find out the corresponding indexes of target columns regarding the given value
-def find_index(df, column, value):
-    return (df.index[df[column] == value])
-
-# @para df:
-#       type: pandas.DataFrame
 #       content: the data frame to be modified
 # @para update_info:
 #       type: list
@@ -63,18 +47,3 @@ def update_and_set(df, update_info, abs_super, details):
     update(df, update_info)
     nl= pd.DataFrame([{"name": update_info[-1][0], "best": update_info[-1][1], "abs_super": abs_super, "details": details}])        # update_info[-1] is a tuple where information of model to be set is stored
     df = pd.concat([df, nl], ignore_index=True)
-
-# @para df:
-#       type: pandas.DataFrame
-#       content: the data frame to be modified
-# @para name:
-#       type: str
-#       content: name of the model to be added
-# @para score:
-#       type: numpy.ndarray
-#       content: a list of floats which are returned from myScore function
-# functionality: create a new line as DataFrame and append it after the last column df in which has the same prefix length
-def add_new_line(df, name, length, score):
-    nl = pd.DataFrame([{"name":name, "prefix_length":length, "MAE":score[0], "RMSE":score[1], "MedAE":score[2], "R2":score[3]}])
-    idx = find_index(df, "prefix_length", length).max()                         # the last row matched
-    df = pd.concat([df.iloc[:idx+1], nl, df.iloc[idx+1:]], ignore_index=True)
